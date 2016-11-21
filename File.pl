c881218dce1f0d0b98fbb7d1501f06335bbcece6
Aly Yakan <alyyakan@hotmail.com>
2016-11-21 21:43:03 +0200

    Documentation

--- File.pl
+++ File.pl
@@ -1,9 +1,15 @@
 mazeConfig(X):- 
- X = [pos(0,0,6,0),pos(0,1,5,0),pos(0,2,6,0),pos(0,3,5,0),pos(1,0,12,0),pos(1,1,10,0),pos(1,2,9,1),pos(1,3,12,0),pos(2,0,8,0),pos(2,1,6,0),pos(2,2,5,0),pos(2,3,12,0),pos(3,0,2,0),pos(3,1,9,1),pos(3,2,10,0),pos(3,3,9,0)].
+ X = [pos(0,0,6,0),pos(0,1,3,1),pos(0,2,5,0),pos(0,3,4,0),pos(1,0,12,0),pos(1,1,6,0),pos(1,2,9,1),pos(1,3,12,1),pos(2,0,8,0),pos(2,1,12,0),pos(2,2,6,0),pos(2,3,13,1),pos(3,0,2,0),pos(3,1,11,0),pos(3,2,9,1),pos(3,3,8,0)].
 
 startPoint(2,0).
 
-endPoint(2,3).
+endPoint(2,1).
 
-timer(15).
+timer(14).
 
+pokemon(0,1,s0).
+pokemon(1,2,s0).
+pokemon(1,3,s0).
+pokemon(2,3,s0).
+pokemon(3,2,s0).
+pokemonLeft(5).
--- project2.pl
+++ project2.pl
@@ -1,5 +1,6 @@
 :- include('File.pl').
 :- use_module(library(clpfd)).
+:- discontiguous(agent/4).
 /*
 North, South, East, West.
 0001, 0010, 0100, 1000.
@@ -10,48 +11,10 @@ North, South, East, West.
 */
 
 /*
-    $ iterative_deepening((legal(S), agent(2, 3, 0, S)),50,R).
-    $ call_with_depth_limit((legal(S), agent(2, 3, 0, S)),50,R).
+    $ iterative_deepening((legal(S), endPoint(X, Y), agent(X, Y, 0, S)),1,R).
 S = result(moveEast, result(moveEast, result(moveSouth, result(moveWest, result(moveSouth, result(moveEast, result(moveSouth, result(moveWest, result(moveWest, result(moveEast, result(moveWest, result(moveEast, result(moveWest, result(moveEast, result(moveWest, s0))))))))))))))),
 R = 34
 */
-
-/* Preconditions for actions */
-position(moveNorth, S):-
-    agent(X, Y, Timer, S),
-    Y1 is Y - 1,
-    %% TODO: can we move forward ?
-    mazeConfig(L),
-    member(pos(X, Y, P, D), L),
-    1 is mod(P, 2). %% It means it can move north.
-
-position(moveSouth, S):-
-    agent(X, Y, Timer, S),
-    Y1 is Y + 1,
-    %% TODO: can we move forward ?
-    mazeConfig(L),
-    member(pos(X, Y, P, D), L),
-    P1 is div(P, 2),
-    1 is mod(P1, 2). %% It means it can move South.
-
-position(moveEast, S):-
-    agent(X, Y, Timer, S),
-    X1 is X + 1,
-    %% TODO: can we move forward ?
-    mazeConfig(L),
-    member(pos(X, Y, P, D), L),
-    P1 is div(P, 4),
-    1 is mod(P1, 2). %% It means it can move East.
-
-position(moveWest, S):-
-    agent(X, Y, Timer, S),
-    X1 is X - 1,
-    %% TODO: can we move forward ?
-    mazeConfig(L),
-    member(pos(X, Y, P, D), L),
-    P1 is div(P, 8),
-    1 is mod(P1, 2). %% It means it can move West.
-
 
 /* Initial Conditions */
 agent(X, Y, Timer, s0):-
@@ -59,8 +22,94 @@ agent(X, Y, Timer, s0):-
     %% TODO: pokemonLeft.
     timer(Timer).
 
-/* Successor-state axioms for fluents */
+/* Preconditions for actions */
+position(moveNorth, S):-
+    /*
+        To be able to moveNorth, we decrease the Y by 1,
+        however, we have to check if the P, which is the
+        available directions from the agent's position, is
+        XXX1 in binary where it can be expressed as:
+            modulus(P, 2) == 1 ?
+    */
+    agent(X, Y, Timer, S),
+    Y1 is Y - 1,
+    mazeConfig(L),
+    member(pos(X, Y, P, D), L),
+    1 is mod(P, 2). %% It means it can move north.
 
+position(moveSouth, S):-
+    /*
+        To be able to moveSouth, P must be XX1X in binary
+        which can be expressed as:
+            P1 = P / 2 and
+            modulus(P1, 2) == 1 ?
+    */
+    agent(X, Y, Timer, S),
+    Y1 is Y + 1,
+    mazeConfig(L),
+    member(pos(X, Y, P, D), L),
+    P1 is div(P, 2),
+    1 is mod(P1, 2). %% It means it can move South.
+
+position(moveEast, S):-
+    /*
+        To be able to moveEast, P must be X1XX in binary
+        which can be expressed as:
+            P1 = P / 4 and
+            modulus(P1, 2) == 1 ?
+    */
+    agent(X, Y, Timer, S),
+    X1 is X + 1,
+    mazeConfig(L),
+    member(pos(X, Y, P, D), L),
+    P1 is div(P, 4),
+    1 is mod(P1, 2). %% It means it can move East.
+
+position(moveWest, S):-
+    /*
+        To be able to moveEast, P must be 1XXX in binary
+        which can be expressed as:
+            P1 = P / 8 and
+            modulus(P1, 2) == 1 ?
+    */
+    agent(X, Y, Timer, S),
+    X1 is X - 1,
+    mazeConfig(L),
+    member(pos(X, Y, P, D), L),
+    P1 is div(P, 8),
+    1 is mod(P1, 2). %% It means it can move West.
+
+position(grabPokemon(X, Y), S):-
+    agent(X, Y, Timer, S),
+    pokemon(X, Y, S).
+
+/* Successor-state axioms for fluents */
+/*
+
+ (robot(x,s) (a avanzar a retroceder))
+ (robot(x 1,s) a retroceder)
+robot(x,do(a,s)) (robot(x 1,s) a avanzar)
+∧ ¬ = ∨ =
++ ∧ = ∨
+↔ − ∧ = ∨
+agent(X, Y, T, result(A, S)) ↔ 
+    [
+        (T1 = T - 1 ∧ biggerThan(T1, 0) ∨ T = 0) ∧
+        (
+            (agent(X - 1, Y, T1, S) ∧ A=moveWest) ∨
+            (agent(X + 1, Y, T1, S) ∧ A=moveEast) ∨
+            (agent(X, Y - 1, T1, S) ∧ A=moveNorth) v∨
+            (agent(X, Y + 1, T1, S) ∧ A=moveSouth)
+        )
+    ] ∨
+    [   
+        agent(X, Y, T, S) ∧
+        ¬(A=moveNorth) ∧
+        ¬(A=moveSouth) ∧
+        ¬(A=moveEast) ∧
+        ¬(A=moveWest)
+    ]
+*/
 agent(C, B, T, result(A,S)):- 
     agent(W, Z, T1, S),
     (
@@ -81,6 +130,11 @@ agent(C, B, T, result(A,S)):-
         \+A=moveEast,
         \+A=moveWest
     ).
+
+pokemon(X, Y, result(A,S)):-
+    A = grabPokemon(X, Y);
+    pokemon(X, Y, S),
+    \+A=grabPokemon(X, Y).
 
 /* legal Axioms */
 legal(s0).
